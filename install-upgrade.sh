124191bd57961f41f0f233916790faf2287dcb3e
jieran233 <jieranjiang233@gmail.com>
2024-07-11 18:51:44 +0800

    .

---
+++ install-upgrade.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SELF_DIR="$(dirname "$(readlink -f "$0")")"
+TARGET_DIR="$HOME/.local/share/icons"
+
+mkdir -p "$TARGET_DIR"
+for _i in OneUI{,-dark,-light}; do
+    _from="$SELF_DIR/$_i"
+    _to="$TARGET_DIR/$_i"
+    echo "Uninstall old version of $_i in $TARGET_DIR"
+    rm -rf "${_to:?}/"*
+    echo "Install $_i to $TARGET_DIR"
+    cp -dr --no-preserve=mode "$SELF_DIR/$_i" "$TARGET_DIR"
+done
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SELF_DIR="$(dirname "$(readlink -f "$0")")"
+TARGET_DIR="$HOME/.local/share/icons"
+
+mkdir -p "$TARGET_DIR"
+for _i in OneUI{,-dark,-light}; do
+    echo "Install $_i to $TARGET_DIR"
+    cp -dr --no-preserve=mode "$SELF_DIR/$_i" "$TARGET_DIR"
+done
